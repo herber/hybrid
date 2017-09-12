01f6b6a1603e5db5d5c3148e0a9d700f6b90637e
Tobias Herber <tobihrbr@gmail.com>
2017-09-12 13:16:24 +0200

    add cli box

--- packages/hybrid-logs/lib/box.js
+++ packages/hybrid-logs/lib/box.js
@@ -2,5 +2,5 @@ const boxen = require('boxen');
 const chalk = require('chalk');
 
 module.exports = (name, more) => {
-  return boxen(chalk`{green ${ name }}\n\n${more}`, { padding: 1, margin: 2, borderColor: 'green' });
+  console.log(boxen(chalk`{green ${ name }}\n\n${more}`, { padding: 1, margin: 2, borderColor: 'green' }));
 };
